fdf9ee1abf7d6ecb3d5a661d7ea506a16d868929
Túlio Dantas <tuliopragana@hotmail.com>
2024-06-11 21:30:44 -0300

    Adição de funções a criação de conta

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,24 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        Locale.setDefault(Locale.US);
+
+        System.out.println("Informe sua conta do banco digitando as informações.");
+        System.out.println("Digite o número da sua agência: ");
+        String agencia = sc.next();
+
+        System.out.println("Usuário: ");
+        int numero = sc.nextInt();
+
+        String nome = "Túlio José Pragana Dantas do Nascimento";
+
+        double saldo = 237.48;
+
+        System.out.println();
+        System.out.printf("Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s," +
+                " conta %d e seu saldo %.2f já está disponível para saque", nome, agencia, numero,saldo);
     }
 }
